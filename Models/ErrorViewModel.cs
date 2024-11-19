cbbd47b1bb5f9a0e8e8682d19511f1d46f525aef
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-11-19 09:08:54 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Client_Applications.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
